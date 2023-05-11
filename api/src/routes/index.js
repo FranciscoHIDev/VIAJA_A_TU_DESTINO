595bd26dc04cb1b8225bc35dec1360348d6ad63b
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-05-11 14:54:39 -0500

    actualizacion base de datos y rutas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 /* This is importing the routes from the files that are in the same directory as this file. */
 const offerRoutes = require("./Offers")
 const userRoutes = require("./Users")
+const destinationRoutes= require("./Destinations")
 
 
 /* Telling the server to use the routes in the files that are imported. */
 router.use("/offers", offerRoutes)
 router.use("/users", userRoutes)
+router.use("/destinations",destinationRoutes)
 
 module.exports = router
