fc6380d6169f14891ff6913e9c4cdf8c3b5ce3c8
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-05-13 22:32:04 -0500

    actualizacion hoteles

--- client/src/components/CardHotels/CardHotels.jsx
+++ client/src/components/CardHotels/CardHotels.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./CardHotel.css";
-import { FaSuitcase, FaGift } from "react-icons/fa";
-import { MdFavoriteBorder, MdLocationOn, MdPeople } from "react-icons/md";
+import { FaHotel } from "react-icons/fa";
+import {
+  MdFavoriteBorder,
+  MdLocationOn,
+  MdPeople,
+  MdLocalHotel,
+} from "react-icons/md";
 import { RiShareForwardLine } from "react-icons/ri";
 
 function CardHotels({
@@ -23,22 +28,30 @@ function CardHotels({
         <MdFavoriteBorder className="favorite" />
       </div>
       <img className="img" src={image} alt="image" />
-      <div className="item-promotion">
-        <p>{discount}</p>
-        <p className="icon-border">
-          <FaGift className="icon" />
-        </p>
-      </div>
+      {discount ? (
+        <div className="item-promotion">
+          <p>{discount}</p>
+          <p className="icon-border">
+            <FaHotel className="icon" />
+          </p>
+        </div>
+      ) : null}
 
       <div>
         <h1 className="title">{name}</h1>
         <div className="items-availability">
-          <p className="availability">
-            Desde <span className="item-date">{from}</span>
-          </p>
-          <p className="availability">
-            Hasta <span className="item-date">{to}</span>
-          </p>
+          <div className="item-hotel">
+            {" "}
+            <MdLocalHotel className="icon-hotel" />
+          </div>
+          <div className="data-availability">
+            <p className="availability">
+              Desde <span className="item-date">{from}</span>
+            </p>
+            <p className="availability">
+              Hasta <span className="item-date">{to}</span>
+            </p>
+          </div>
         </div>
         <div className="item-persons">
           <MdPeople className="persons" />
@@ -52,8 +65,13 @@ function CardHotels({
       <div className="items-price">
         <div className="price-data">
           <p className="price">Desde </p>
-          <p className="price-prev">${previousPrice}</p>
-          <p className="colorprice"> ${price} </p>
+          {previousPrice ? (
+            <p className="price-prev">${previousPrice}</p>
+          ) : (
+            <br></br>
+          )}
+
+          <p className="colorprice"> ${price}</p>
           <p className="text-taxes">Impuestos incluidos</p>
         </div>
         <div className="button-buy">
