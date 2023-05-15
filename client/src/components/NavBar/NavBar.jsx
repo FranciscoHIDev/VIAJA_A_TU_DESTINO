6b636736e767325ac48cc9f1174b4712673c1811
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-05-15 13:59:33 -0500

    avance en el menu

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-  return <div className="flex text-4xl">NavBar</div>;
+  return (
+    <>
+      <nav>
+        <div className="navbar">
+          <div className="header-container">
+            <img
+              className=""
+              src="https://res.cloudinary.com/duaysiozi/image/upload/v1684173882/LOGO-OFICIAL-_lmdwby.svg"
+              alt="logo-oficial"
+            />
+            <div className="header-info">
+              <p>Contactanos +52 998 283 0657 </p>
+              <p>Iniciar Sesión</p>
+              <p>Favoritos</p>
+              <p>Ayuda</p>
+            </div>
+          </div>
+          <div className="header-navegation">
+            <ul>
+              <li>
+                <Link to="/home">Ofertas</Link>
+              </li>
+              <li>
+                <Link to="/home">Paquetes</Link>
+              </li>
+              <li>
+                <Link to="/home">Hoteles</Link>
+              </li>
+              <li>
+                <Link to="/home">Vuelos</Link>
+              </li>
+              <li>
+                <Link to="/home">Tours</Link>
+              </li>
+              <li>
+                <Link to="/home">Blog</Link>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default NavBar;
