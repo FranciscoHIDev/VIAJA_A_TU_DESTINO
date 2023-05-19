10f3ca24fc7ab17a2935a9eb73c29eaee1cfcfff
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-05-19 00:28:59 -0500

    actualizacion de card

--- client/src/components/CardHotel/CardHotel.jsx
+++ client/src/components/CardHotel/CardHotel.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./CardHotel.css";
 import { FaHotel } from "react-icons/fa";
 import {
   MdFavoriteBorder,
@@ -22,61 +21,78 @@ function CardHotel({
   link,
 }) {
   return (
-    <div className="conteiner-hotel">
+    <div className="mt-[30px] rounded-[20px] bg-[#fff] w-[390px] h-[465px] border-none m-[15px] ">
       <div>
-        <RiShareForwardLine className="share" />
-        <MdFavoriteBorder className="favorite" />
+        <div>
+          <RiShareForwardLine className="cursor-pointer bg-[hsla(0,0%,100%,.5)] rounded-[50%] absolute text-[25px] ml-[300px] mt-[20px] text-[#323231] opacity-[80%]" />
+          <MdFavoriteBorder className="cursor-pointer bg-[hsla(0,0%,100%,.5)] rounded-[50%] absolute text-[25px] ml-[340px] mt-[20px] text-[#323231] opacity-[80%]" />
+        </div>
+        <img
+          className="w-[390px] h-[200px] rounded-tl-[20px] rounded-tr-[20px]"
+          src={image}
+          alt="image"
+        />
+        {discount ? (
+          <div className="flex flex-row items-center absolute mt-[-180px] rounded-tr-[100px] rounded-br-[100px] pl-[10px] h-[50px] text-[1em] font-[500]  bg-[#f4991a] text-[#fff]">
+            <p className="text-[19px] drop-shadow-[0.1em_0.1em_0.1em_black]">
+              {discount}
+            </p>
+            <p className="ml-[10px] bg-[#fff] rounded-[100%] w-[50px] h-[50px]">
+              <FaHotel className="text-[#035373] text-[32px] ml-[9px] mt-[8px]" />
+            </p>
+          </div>
+        ) : null}
       </div>
-      <img className="img-hotel" src={image} alt="image" />
-      {discount ? (
-        <div className="item-promotion-hotel">
-          <p className="discount-hotel">{discount}</p>
-          <p className="icon-border-hotel">
-            <FaHotel className="icon-hotel-promo" />
-          </p>
-        </div>
-      ) : null}
-
       <div>
-        <h1 className="title-hotel">{name}</h1>
-        <div className="items-availability-hotel">
-          <div className="item-hotel">
+        <h1 className="text-[18px] text-[#035373] mx-[20px] mt-2">{name}</h1>
+        <div className="flex flex-row mt-2">
+          <div className="mt-[20px] ml-[20px]">
             {" "}
-            <MdLocalHotel className="icon-hotel" />
+            <MdLocalHotel className="text-[19px] text-[#323231] mt-[-16px] " />
           </div>
-          <div className="data-availability-hotel">
-            <p className="availability-hotel">
-              Desde <span className="item-date-hotel">{from}</span>
+          <div>
+            <p className="ml-[10px] text-[#323231] font-[300]">
+              Desde{" "}
+              <span className="text-[15px] font-[600] text-[#323231]">
+                {from}
+              </span>
             </p>
-            <p className="availability-hotel">
-              Hasta <span className="item-date-hotel">{to}</span>
+            <p className="ml-[10px] text-[#323231] font-[300]">
+              Hasta{" "}
+              <span className="text-[15px] font-[600] text-[#323231] ml-1">
+                {to}
+              </span>
             </p>
           </div>
         </div>
-        <div className="item-persons-hotel">
-          <MdPeople className="persons-hotel" />
+        <div className="flex flex-row ml-[20px] items-center mt-2">
+          <MdPeople className="mr-[10px] text-[20px] text-[#323231]" />
           <p>{persons} personas</p>
         </div>
-        <div className="items-destination-hotel">
-          <MdLocationOn className="icon-location-hotel" />
-          <p className="destination-hotel">{destination.name}</p>
+        <div className="flex flex-row items-center ml-[20px] mt-2">
+          <MdLocationOn className="text-[#ff3302] text-[20px] mr-[10px]" />
+          <p className="text-[15px] font-[600] text-[#323231] drop-shadow-[0.1em_0.1em_0.4em_#ff3e02]">
+            {destination.name}
+          </p>
         </div>
       </div>
-      <div className="items-price-hotel">
-        <div className="price-data-hotel">
-          <p className="price-hotel">Desde </p>
+      <div className="flex flex-row-reverse m-[30px] justify-between">
+        <div className="flex flex-col items-center mt-[-50px]">
+          <p className="text-[15px]">Desde </p>
           {previousPrice ? (
-            <p className="price-prev-hotel">${previousPrice}</p>
+            <p className="text-[20px] line-through">${previousPrice}</p>
           ) : (
             <br></br>
           )}
 
-          <p className="colorprice-hotel"> ${price}</p>
-          <p className="text-taxes-hotel">Impuestos incluidos</p>
+          <p className="text-[#ff3e02] font-bold text-[20px]"> ${price}</p>
+          <p className="text-[14px] text-[#323231]">Impuestos incluidos</p>
         </div>
-        <div className="button-buy-hotel">
+        <div className="flex  text-[#323231]">
           <a href={link} target="_blank" rel="noopener noreferrer">
-            <button>Reservar</button>
+            <button className="rounded-md border border-[#242424] bg-[#ff] py-[0.4em] px-[1.2em]   hover:bg-[#ff3e02] hover:text-[#fff] hover:border-[#ff3e02]">
+              Reservar
+            </button>
           </a>
         </div>
       </div>
--- client/src/components/CardOffers/CardOffers.jsx
+++ client/src/components/CardOffers/CardOffers.jsx
@@ -26,68 +26,88 @@ function CardOffers({
   buyLinks,
 }) {
   return (
-    <div className="conteiner-offer">
+    <div className="mt-[30px] rounded-[20px] bg-[#fff] w-[390px] h-[490px] m-[15px] border-none ">
       <div>
-        <RiShareForwardLine className="share" />
-        <MdFavoriteBorder className="favorite" />
-        <img className="img-offer" src={image} alt="image" />
-        <p className="buttonromotion-offer">{promotion}</p>
-      </div>
-      <div className="items-category-offer">
+        <RiShareForwardLine className="cursor-pointer bg-[hsla(0,0%,100%,.5)] rounded-[50%] absolute text-[25px] ml-[300px] mt-[20px] text-[#323231] opacity-[80%]" />
+        <MdFavoriteBorder className="cursor-pointer bg-[hsla(0,0%,100%,.5)] rounded-[50%] absolute text-[25px] ml-[340px] mt-[20px] text-[#323231] opacity-[80%]" />
         <img
-          className="img-category-offer"
+          className="w-[390px] h-[200px] rounded-tl-[20px] rounded-tr-[20px]"
+          src={image}
+          alt="image"
+        />
+        <p className="absolute mt-[-180px] rounded-tr-[8px] rounded-br-[8px] px-[1.2em] py-[0.6em] text-[1em] font-[500] bg-[hsla(0,0%,100%,.8)] text-[#323231]">
+          {promotion}
+        </p>
+      </div>
+      <div className="flex flex-row items-center mt-2">
+        <img
+          className="w-[20px] ml-[20px] mr-[10px]"
           src={category.image}
           alt={category.name}
         />
-        <p className="category-offer">{category.name}</p>
+        <p className="text-[20px] text-[#323231]">{category.name}</p>
       </div>
       <div>
         <div>
-          <h1 className="title-offer">{title}</h1>
-          <p className="summary-offer">{summary}</p>
-          <div className="item-availability-offer">
-            <FaRegCalendarAlt className="icons-color" />
-            <p className="availability-offer">
+          <h1 className="text-[18px] text-[#035373] ml-[20px] font-[600]">
+            {title}
+          </h1>
+          <p className="ml-[20px] mb-3 ">{summary}</p>
+          <div className="flex flex-row items-center ml-[20px]">
+            <FaRegCalendarAlt className="text-[#ff3e02]" />
+            <p className="ml-[10px] text-[#323231] font-[400] ">
               Disponibilidad:{" "}
-              <span className="item-date-offer">{availability}</span>
+              <span className="text-[15px] font-[600] text-[#323231]">
+                {availability}
+              </span>
             </p>
           </div>
           {departure ? (
-            <div className="item-departure-offer">
-              <FaPlaneDeparture className="icons-color" />
-              <p className="departure-offer">
+            <div className="flex flex-row items-center ml-[20px]">
+              <FaPlaneDeparture className="text-[#ff3e02]" />
+              <p className="ml-[10px] text-[#323231] font-[400]">
                 Salida:{" "}
-                <span className="location-departure-offer">{departure}</span>
+                <span className="text-[15px] font-[600] text-[#323231]">
+                  {departure}
+                </span>
               </p>
             </div>
           ) : null}
         </div>
         {arrival ? (
-          <div className="item-arrival-offer">
-            <FaPlaneArrival className="icons-color" />
-            <p className="arrival-offer">
+          <div className="flex flex-row items-center ml-[20px]">
+            <FaPlaneArrival className="text-[#ff3e02]" />
+            <p className="ml-[10px] text-[#323231] font-[400]">
               Llegada:{" "}
-              <span className="location-departure-offer">{arrival}</span>
+              <span className="text-[15px] font-[600] text-[#323231]">
+                {arrival}
+              </span>
             </p>
           </div>
         ) : (
           <br></br>
         )}
-
         {category.name === "Hotel" ? (
-          <div className="items-destination-offer">
-            <MdLocationOn className="icon-location-offer" />
-            <p className="destination-offer">{destination.name}</p>
+          <div className="flex flex-row items-center ml-[20px] mt-[-20px] ">
+            <MdLocationOn className="text-[#ff3e02] mr-[5px] text-[20px]" />
+            <p className="text-[15px] font-[600] text-[#323231]">
+              {destination.name}
+            </p>
           </div>
         ) : null}
       </div>
-      <div className="button-offer">
-        <p className="price-offer">
+      <div className="flex flex-row-reverse justify-between items-center m-[30px] ">
+        <p className="text-[15px]">
           Desde
-          <span className="colorprice-offer"> ${price} </span>
+          <span className="text-[#ff3e02] font-bold text-[20px]">
+            {" "}
+            ${price}{" "}
+          </span>
         </p>
         <a href={buyLinks} target="_blank" rel="noopener noreferrer">
-          <button>Ver oferta</button>
+          <button className="rounded-md border border-[#242424] bg-[#ff] py-[0.4em] px-[1.2em]   hover:bg-[#ff3e02] hover:text-[#fff] hover:border-[#ff3e02]">
+            Ver oferta
+          </button>
         </a>
       </div>
     </div>
