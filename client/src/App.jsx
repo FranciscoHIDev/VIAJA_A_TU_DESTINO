a50a7955d1f7491c59e2a0138660482f8054a4fb
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-06-02 00:17:51 -0500

    page 404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import FrequentQuestions from "./pages/FrequentQuestions/FrequentQuestions";
@@ -6,22 +6,27 @@ import NoticeOfPrivacy from "./pages/NoticeOfPrivacy/NoticeOfPrivacy";
 import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions/TermsAndConditions";
 import Details from "./pages/Details/Details";
+import NotFound from "./pages/NotFound/NotFound";
+
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/oferta/:id" element={<Details />} />
-        <Route path="/sobre-nosotros" element={<About />} />
-        <Route path="/preguntas-frecuentes" element={<FrequentQuestions />} />
-        <Route path="/aviso-de-privacidad" element={<NoticeOfPrivacy />} />
-        <Route path="/politica-de-privacidad" element={<PrivacyPolicy />} />
-        <Route
-          path="/terminos-y-condiciones"
-          element={<TermsAndConditions />}
-        />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route exact path="/oferta/:id" element={<Details />} />
+          <Route path="/sobre-nosotros" element={<About />} />
+          <Route path="/preguntas-frecuentes" element={<FrequentQuestions />} />
+          <Route path="/aviso-de-privacidad" element={<NoticeOfPrivacy />} />
+          <Route path="/politica-de-privacidad" element={<PrivacyPolicy />} />
+          <Route
+            path="/terminos-y-condiciones"
+            element={<TermsAndConditions />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/pages/Details/Details.jsx
+++ client/src/pages/Details/Details.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import Loading from "../../components/Loading/Loading";
 import NavBar from "../../components/NavBar/NavBar";
