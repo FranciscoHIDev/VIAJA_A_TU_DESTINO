1b27616a415e0d88a5c802348c9283c3d47e04d2
Isidoro Francisco Hilario <ihilario00@gmail.com>
2024-01-09 14:53:47 +0000

    cambios favoritos en perfil de usuario

--- client/src/LayoutProfile/LayoutProfile.jsx
+++ client/src/LayoutProfile/LayoutProfile.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/Footer/Footer";
 import { FaRegUserCircle } from "react-icons/fa";
 import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { HiOutlineBellAlert } from "react-icons/hi2";
 import { CiDiscount1 } from "react-icons/ci";
 import { MdOutlineAdsClick } from "react-icons/md";
 import { BsCartCheck } from "react-icons/bs";
-import NavBar from "../components/NavBar/NavBar";
-import Footer from "../components/Footer/Footer";
+import { NavLink } from "react-router-dom";
+
 
 function LayoutProfile() {
   return (
@@ -17,37 +19,49 @@ function LayoutProfile() {
         </header>
         <main className="flex-grow pt-20 pb-40">
           <div className="flex flex-row mx-40  justify-center">
-            <div className="flex flex-row items-center bg-white border w-full py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
+            <NavLink to={"/mi-perfil/datos-personales"}>
+            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
               <FaRegUserCircle className="text-3xl " />
               <p className="ml-3">Datos personales</p>
             </div>
-            <div className="flex flex-row items-center bg-white border w-full py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
+         </NavLink>
+       <NavLink to={"/mi-perfil/alertas"}>
+            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
               <HiOutlineBellAlert className="text-3xl " />
               <p className="ml-3">Mis alertas</p>
             </div>
+          </NavLink>
           </div>
           <div className="flex flex-row mx-40  justify-center mt-5">
-            <div className="flex flex-row items-center bg-white border w-full py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
+          <NavLink to={"/mi-perfil/descuentos"}>
+            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
               <CiDiscount1 className="text-3xl " />
               <p className="ml-3">Mis descuentos</p>
             </div>
-            <div className="flex flex-row items-center bg-white border w-full py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
+            </NavLink>
+            <NavLink to={"/mi-perfil/favoritos"}>
+            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
               <MdOutlineFavoriteBorder className="text-3xl " />
               <p className="ml-3">Mis ofertas favoritas</p>
             </div>
+            </NavLink>
           </div>
           <div className="flex flex-row mx-40  justify-center mt-5">
-            <div className="flex flex-row items-center bg-white border w-full py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
+          <NavLink to={"/mi-perfil/reservas"}>
+            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
               <BsCartCheck className="text-3xl " />
               <p className="ml-3">Mis reservas</p>
             </div>
-            <div className="flex flex-row items-center bg-white border w-full py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
+            </NavLink>
+            <NavLink to={"/mi-perfil/ultimas-ofertas"}>
+            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
               <MdOutlineAdsClick className="text-3xl " />
               <p className="ml-3">Últimas ofertas visitadas</p>
             </div>
+            </NavLink>
           </div>
           <div className="flex flex-row mx-48 mt-4 justify-end ">
-            <button className="">Cerrar Sesion</button>
+            <button className="text-[#ff3e02] underline">Cerrar Sesion</button>
           </div>
         </main>
         <footer>
--- client/src/LayoutProfile/pages/Favorites.jsx
+++ client/src/LayoutProfile/pages/Favorites.jsx
@@ -3,6 +3,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { getAllUsers } from "../../redux/actions/actions";
 import CardFavorite from "../../components/CardFavorite/CardFavorite";
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
 
 function Favorites() {
   const { user } = useAuth0();
@@ -18,7 +20,9 @@ function Favorites() {
 
   return (
     <>
-      <div className="flex  flex-col text-3xl text-black">
+    <NavBar/>
+    <main className="flex-grow pt-20 pb-40">
+      <div className="flex  flex-col text-3xl text-black items-center">
         <div>
           <h1>¡Bienvenido al mejor buscador de ofertas de viajes!</h1>
         </div>
@@ -49,6 +53,8 @@ function Favorites() {
           )}
         </div>
       </div>
+      </main>
+      <Footer/>
     </>
   );
 }
