8510d7ddd751018e4d749a206b3ed54fec552e52
FranciscoHIDev <ihilario00@gmail.com>
2024-01-13 23:08:23 -0500

    actualizacion perfil usuario

--- client/src/LayoutProfile/LayoutProfile.jsx
+++ client/src/LayoutProfile/LayoutProfile.jsx
@@ -8,66 +8,70 @@ import { CiDiscount1 } from "react-icons/ci";
 import { MdOutlineAdsClick } from "react-icons/md";
 import { BsCartCheck } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
-import Breadcrumb from "../components/Breadcrumb/Breadcrumb"
-
+import LogoutButton from "../components/Auth0/LogoutButton";
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
 
 function LayoutProfile() {
+  // const { isAuthenticated, user } = useAuth0();
   const crumbs = [
-    { label: 'Inicio', path: '/' },
-    { label: 'Mi perfil', path: '/mi-perfil' }
-  ]
+    { label: "Inicio", path: "/" },
+    { label: "Mi perfil", path: "/mi-perfil" },
+  ];
   return (
     <React.Fragment>
       <div className="min-h-screen h-screen flex flex-col">
         <header>
           <NavBar />
         </header>
-        <Breadcrumb crumbs={crumbs} />
+        <div className="ml-20 mt-5">
+          <Breadcrumb crumbs={crumbs} />
+        </div>
+
         <main className="flex-grow pt-20 pb-40">
           <div className="flex flex-row mx-40  justify-center">
             <NavLink to={"/mi-perfil/datos-personales"}>
-            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
-              <FaRegUserCircle className="text-3xl " />
-              <p className="ml-3">Datos personales</p>
-            </div>
-         </NavLink>
-       <NavLink to={"/mi-perfil/alertas"}>
-            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
-              <HiOutlineBellAlert className="text-3xl " />
-              <p className="ml-3">Mis alertas</p>
-            </div>
-          </NavLink>
+              <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
+                <FaRegUserCircle className="text-3xl " />
+                <p className="ml-3">Datos personales</p>
+              </div>
+            </NavLink>
+            <NavLink to={"/mi-perfil/alertas"}>
+              <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
+                <HiOutlineBellAlert className="text-3xl " />
+                <p className="ml-3">Mis alertas</p>
+              </div>
+            </NavLink>
           </div>
           <div className="flex flex-row mx-40  justify-center mt-5">
-          <NavLink to={"/mi-perfil/descuentos"}>
-            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
-              <CiDiscount1 className="text-3xl " />
-              <p className="ml-3">Mis descuentos</p>
-            </div>
+            <NavLink to={"/mi-perfil/descuentos"}>
+              <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
+                <CiDiscount1 className="text-3xl " />
+                <p className="ml-3">Mis descuentos</p>
+              </div>
             </NavLink>
             <NavLink to={"/mi-perfil/favoritos"}>
-            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
-              <MdOutlineFavoriteBorder className="text-3xl " />
-              <p className="ml-3">Mis ofertas favoritas</p>
-            </div>
+              <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
+                <MdOutlineFavoriteBorder className="text-3xl " />
+                <p className="ml-3">Mis ofertas favoritas</p>
+              </div>
             </NavLink>
           </div>
           <div className="flex flex-row mx-40  justify-center mt-5">
-          <NavLink to={"/mi-perfil/reservas"}>
-            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
-              <BsCartCheck className="text-3xl " />
-              <p className="ml-3">Mis reservas</p>
-            </div>
+            <NavLink to={"/mi-perfil/reservas"}>
+              <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5 rounded-md mr-8 cursor-pointer hover:border-stone-950">
+                <BsCartCheck className="text-3xl " />
+                <p className="ml-3">Mis reservas</p>
+              </div>
             </NavLink>
             <NavLink to={"/mi-perfil/ultimas-ofertas"}>
-            <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
-              <MdOutlineAdsClick className="text-3xl " />
-              <p className="ml-3">Últimas ofertas visitadas</p>
-            </div>
+              <div className="flex flex-row items-center bg-white border w-[400px] py-8 pl-5  rounded-md mr-8 cursor-pointer hover:border-stone-950">
+                <MdOutlineAdsClick className="text-3xl " />
+                <p className="ml-3">Últimas ofertas visitadas</p>
+              </div>
             </NavLink>
           </div>
-          <div className="flex flex-row mx-48 mt-4 justify-end ">
-            <button className="text-[#ff3e02] underline">Cerrar Sesion</button>
+          <div className="flex flex-row mt-4 pr-[277px] justify-end text-[#ff3e02] underline font-medium">
+            <LogoutButton />
           </div>
         </main>
         <footer>
--- client/src/components/Breadcrumb/Breadcrumb.jsx
+++ client/src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 const Breadcrumb = ({ crumbs }) => {
   return (
-    <div className="breadcrumb">
+    <div>
       {crumbs.map((crumb, index) => (
         <span key={index}>
-          {index > 0 && ' > '}
+          {index > 0 && " > "}
           {index < crumbs.length - 1 ? (
-            <Link to={crumb.path}>{crumb.label}</Link>
+            <Link to={crumb.path} className="text-[#ff3e02] font-medium">
+              {crumb.label}
+            </Link>
           ) : (
-            crumb.label
+            <span className="text-[#035373] font-medium">{crumb.label}</span>
           )}
         </span>
       ))}
