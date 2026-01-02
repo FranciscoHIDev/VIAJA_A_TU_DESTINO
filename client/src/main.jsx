9fe1e19ad6524a5a437495a44543141ee2e4d168
Isidoro de Viaje <francisco@MacBook-Air-de-Isidoro.local>
2026-01-01 21:16:34 -0500

    Nueva cuenta auth0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,23 +2,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import axios from "axios";
+import { StrictMode } from 'react';
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 //axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.baseURL = "https://viaja-a-tu-destino-back.vercel.app/";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+     <StrictMode>
     <Auth0Provider
-      domain="dev-diy5j51e8e7m2tec.us.auth0.com"
-      clientId="f8Jjj4658bzQcB9JczyCXILdXgqtNzXV"
+     domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
     >
       <App />
     </Auth0Provider>
+    </StrictMode>
   </Provider>
 );
