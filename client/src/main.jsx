a63742572eeba4a5918d66d2ffe514a2de98e0a6
Isidoro de Viaje <francisco@MacBook-Air-de-Isidoro.local>
2026-01-01 22:08:22 -0500

    cambios cards

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import axios from "axios";
-import { StrictMode } from 'react';
+import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import { Auth0Provider } from "@auth0/auth0-react";
@@ -12,16 +12,16 @@ axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-     <StrictMode>
-    <Auth0Provider
-     domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <StrictMode>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </StrictMode>
   </Provider>
 );
